3c9da6ac1f2dc99db04f3aca196d43b429fe8064
leosatan-fairy <tikhonovich0220@gmail.com>
2020-06-22 23:21:18 +1000

    feat: add interaction between parent and child components

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-product-alerts',
   templateUrl: './product-alerts.component.html',
-  styleUrls: ['./product-alerts.component.scss']
+  styleUrls: ['./product-alerts.component.scss'],
 })
 export class ProductAlertsComponent implements OnInit {
+  @Input() product;
+  @Output() notify = new EventEmitter();
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -16,6 +16,10 @@ export class ProductListComponent implements OnInit {
     window.alert('The product has been shared!');
   }
 
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+
   ngOnInit(): void {
   }
 
