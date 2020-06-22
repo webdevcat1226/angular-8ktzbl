9d44918bd5155a984dfc9a87f0db23ed282916f2
leosatan-fairy <tikhonovich0220@gmail.com>
2020-06-22 23:58:32 +1000

    feat: product details view per product added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {ProductListComponent} from './product-list/product-list.component';
+import {ProductDetailsComponent} from './product-details/product-details.component';
 
 
 const routes: Routes = [
   {
-    path:'', component: ProductListComponent
+    path: '', component: ProductListComponent,
+  },
+  {
+    path: 'products/:productId', component: ProductDetailsComponent,
   },
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductAlertsComponent } from './product-list/product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
     ProductListComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
     BrowserModule,
